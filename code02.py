e1505d38863626d811d0b40c076e468fe5705d13
hungtao <taipeihugo@users.noreply.github.com>
2024-10-16 13:59:31 +0800

    Update code02.py

--- code02.py
+++ code02.py
@@ -33,42 +33,28 @@ def linebot():
      # 取得收到的訊息內容
     body = request.get_data(as_text=True)
     try:
-        # json 格式化訊息內容
-        json_data = json.loads(body)
+        json_data = json.loads(body) # json 格式化訊息內容
         
-        # access_token = '你的 LINE Channel access token'
-        # secret = '你的 LINE Channel secret'
         access_token = os.getenv('CHANNEL_ACCESS_TOKEN')
         secret = os.getenv('CHANNEL_SECRET')
-         
-         # 確認 token 是否正確
-        line_bot_api = LineBotApi(access_token)
-        # 確認 secret 是否正確
-        handler = WebhookHandler(secret)
-        # 加入回傳的 headers
-        signature = request.headers['X-Line-Signature']
-        # 綁定訊息回傳的相關資訊
-        handler.handle(body, signature)
-        # 取得回傳訊息的 Token
-        tk = json_data['events'][0]['replyToken']
-        # 取得 LINe 收到的訊息類型
-        type = json_data['events'][0]['message']['type']
+                
+        line_bot_api = LineBotApi(access_token) # 確認 token 是否正確
+        handler = WebhookHandler(secret)        # 確認 secret 是否正確
+        signature = request.headers['X-Line-Signature'] # 加入回傳的 headers
+        handler.handle(body, signature)           # 綁定訊息回傳的相關資訊
+        tk = json_data['events'][0]['replyToken'] # 取得回傳訊息的 Token
+        type = json_data['events'][0]['message']['type'] # 取得 LINe 收到的訊息類型
         if type=='text':
-            # 取得 LINE 收到的文字訊息
-            msg = json_data['events'][0]['message']['text']
-            # 印出內容
-            print(msg)
+            msg = json_data['events'][0]['message']['text'] # 取得 LINE 收到的文字訊息
+            print(msg) # 印出內容
             reply = msg
         else:
             reply = '你傳的不是文字呦～'
         print(reply)
-        # 回傳訊息
-        line_bot_api.reply_message(tk,TextSendMessage(reply))
+        line_bot_api.reply_message(tk,TextSendMessage(reply)) # 回傳訊息
     except:
-        # 如果發生錯誤，印出收到的內容
-        print(body)
-    # 驗證 Webhook 使用，不能省略
-    return 'OK'
+        print(body) # 如果發生錯誤，印出收到的內容
+    return 'OK' # 驗證 Webhook 使用，不能省略
 
 if __name__ == "__main__":
     app.run()
