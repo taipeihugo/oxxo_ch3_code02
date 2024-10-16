db3387f839f8cf691b94da4d11c467ae25be51d0
hungtao <taipeihugo@users.noreply.github.com>
2024-10-16 13:33:20 +0800

    Update code02.py

--- code02.py
+++ code02.py
@@ -22,7 +22,7 @@ def home():
 # 忽略對 favicon.ico 的請求（避免 favicon.ico 的 404 錯誤 ）
 @app.route("/favicon.ico")
 def favicon():
-    return url_for('\', filename='data:,')
+    return send_file('favicon.ico')
 
 @app.route("/", methods=['POST'])
 def linebot():
